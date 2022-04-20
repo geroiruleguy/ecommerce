1e753e3393d020bdafac10de0f8e6a4a9319a1dd
Rama D'Accorso <ramiro.daccorso@gmail.com>
2022-04-19 21:04:07 -0300

    Correcciones

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,40 +1,29 @@
 import React from "react";
 
+export const ItemDetail = ({producto}) => {
 
+  if(!producto){
+    return 'No hay nada'
+  }
 
+  return(
+      <div style={styles}>
+        <img src={producto.img} alt={producto.title} />
+        <h1>{producto.title}</h1>
+        <span>${producto.price}</span>
+        <p>{producto.description}</p>
+      </div>
+    );
+};
 
+const styles = {
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "top",
+  marginLeft: 250,
+  marginRight: 250,
+  marginTop: 50,
+  marginBottom: 100,          
+  backgroundColor: "white", 
 
-
-
-
-export const ItemDetail = (producto) => {
-    const styles = {
-          
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "top",
-          marginLeft: 250,
-          marginRight: 250,
-          marginTop: 50,
-          marginBottom: 100,          
-          backgroundColor: "white", 
-    
-      };
-      
-     
-      
-      
-        return(
-
-          <div style={styles}>
-            <img src={producto.img} alt={producto.title} />
-            <h1>{producto.title}</h1>
-            <span>${producto.price}</span>
-            <p>{producto.description}</p>
-        </div>
-
-
-
-         
-        );
 };
